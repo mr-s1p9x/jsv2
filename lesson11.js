d2906d4b4f994d635a93acc1059412e678fd1f7f
s1pex <temazex777@gmail.com>
2023-09-07 13:33:01 +0300

    lesson20

--- lesson11.js
+++ lesson11.js
@@ -1,5 +1,10 @@
 // Перепресвоение переменных и обновление через let
-let a = 1;
+// let a = 1;
+// console.log(a);
+// a = 'test';
+// console.log(a);
+
+let a = 2;
 console.log(a);
-a = 'test';
+a = 'beQuiet';
 console.log(a);
